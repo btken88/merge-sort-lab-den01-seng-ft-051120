c31cfb9a6584c2992917210c37ccec17e7451ef1
Bryce Kennedy <btken88@gmail.com>
2020-09-09 14:44:09 -0600

    Solve all function requirements.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+function findMinAndRemoveSorted(array) {
+  return array.shift()
+}
+
+function merge(firstHalf, secondHalf) {
+  let sortedArray = []
+  while (firstHalf.length || secondHalf.length) {
+    if (firstHalf[0] < secondHalf[0]) {
+      sortedArray.push(findMinAndRemoveSorted(firstHalf))
+    } else {
+      sortedArray.push(findMinAndRemoveSorted(secondHalf))
+    }
+  }
+  return sortedArray
+}
+
+function mergeSort(array) {
+  if (array.length < 2) {
+    return array
+  } else {
+    let firstHalf = array.slice(0, array.length / 2)
+    let secondHalf = array.slice(array.length / 2)
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+}
